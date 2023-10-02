869d1da135d80a65992697d35c96cd4f5aa33370
Winny <winny.chelangat2@student.moringaschool.com>
2023-10-02 11:07:02 +0300

    Completed

--- server/app.py
+++ server/app.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 
 from flask import request, session
 from flask_restful import Resource
@@ -19,25 +18,54 @@ class Signup(Resource):
     
     def post(self):
         json = request.get_json()
-        user = User(
-            username=json['username'],
-            password_hash=json['password']
-        )
-        db.session.add(user)
-        db.session.commit()
-        return user.to_dict(), 201
+        username = json['username']
+        password = json['password']
+        if username and password:
+            user = User(
+                username=username
+            )
+            user.password_hash = password
+            db.session.add(user)
+            db.session.commit()
+
+            session['user_id'] = user.id
+
+            return user.to_dict(), 201
+        else:
+            return {'error': '422'}, 422
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if session.get('user_id'):
+            user = User.query.filter(User.id == session['user_id']).first()
+            return user.to_dict(), 200
+        else:
+            return {}, 204
 
 class Login(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+        username = json['username']
+        password = json['password']
+        user = User(
+            username=username
+        )
+        user.password_hash = password
+
+        return user.to_dict(), 201
+        
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
+        return {'message': '204: No Content'}, 204
+
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
